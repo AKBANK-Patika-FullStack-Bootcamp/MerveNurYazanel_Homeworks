bf728a09a8d21fb8c8993fcd52ab754fda35ccbc
merve nur yazanel <54467555+mervenuryazanel@users.noreply.github.com>
2022-01-25 08:03:19 +0100

    deleted

--- example1/Model/Result.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace example1
-//{
-  
-//    public class Result
-//    {
-//        public int Status { get; set; }
-//        public string Message { get; set; }
-//    }
-//}
--- example1/Models/Result.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace example1.Models
-{
-  
-    public class Result
-    {
-        public int Status { get; set; }
-        public string Message { get; set; }
-    }
-}
