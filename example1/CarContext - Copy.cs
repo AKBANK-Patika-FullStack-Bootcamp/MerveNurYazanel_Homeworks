bd1b8f94bc2de84bfa6e831e071f2edf6e36cfa2
merve nur yazanel <54467555+mervenuryazanel@users.noreply.github.com>
2022-01-25 07:40:58 +0100

    Week4 added

---
+++ example1/CarContext - Copy.cs
@@ -0,0 +1,58 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using System.Diagnostics.CodeAnalysis;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.Extensions.Configuration;
+
+//namespace example1
+//{
+//    public class CarContext : DbContext
+//    {
+//        public CarContext(DbContextOptions<CarContext> options)
+//            : base(options)
+//        {
+//        }
+
+//        public DbSet<CarContext> Cars { get; set; } = null!;
+
+//        //  public DbSet<Car> Cars { get; set; } = null!;
+//        //public DbSet<User> Users { get; set; } = null!;
+
+//    }
+
+
+//    /*public class CarContext : DbContext
+//    {
+//        protected readonly IConfiguration Configuration;
+//        public void CarContext
+//        {
+
+//        }
+//        //public CarContext(DbContextOptions<CarContext> options)
+//    //: base(options)
+//    //    { }
+
+//        public DbSet<CarContext> Cars { get; set; } = null!;
+//        protected override void OnConfiguring(DbContextOptionsBuilder options)
+//        {
+//            //options.UseSqlServer(Configuration.GetConnectionString("UserDBEntities"));
+//            options.UseSqlServer("Data Source = localhost; Database = RentCarDB; integrated security = True;");
+
+//        }
+//        protected override void OnModelCreating(ModelBuilder modelBuilder)
+//        {
+//            modelBuilder.Entity<Car>().ToTable("Car");
+//            modelBuilder.Entity<User>().ToTable("User");
+//        }
+//        public DbSet<Car> Car { get; set; }
+//        public DbSet<User> User { get; set; }
+//    }*/
+//}
+///*
+// * In ASP.NET Core, services such as the DB context must be registered 
+// * with the dependency injection (DI) container. 
+// * The container provides the service to controllers.  To do this go to --> Program.cs
+// */
---
+++ example1/Models/Result.cs
@@ -0,0 +1,9 @@
+﻿namespace example1.Models
+{
+  
+    public class Result
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+    }
+}
